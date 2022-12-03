0033f24cce1925f24d5f74ffaf5b9a18eb2b3d49
jomasoares <jomamts@gmail.com>
2022-12-03 15:05:23 -0300

    chore: innital commit

---
+++ src/main/java/com/jomasoares/pokecrud/PokecrudApplication.java
@@ -0,0 +1,13 @@
+package com.jomasoares.pokecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokecrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jomasoares/pokecrud/PokecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jomasoares.pokecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
