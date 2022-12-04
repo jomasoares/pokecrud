b7d5e71a07fd41f99755d74bed91d2a0fdd3b8b9
jomasoares <jomamts@gmail.com>
2022-12-04 10:48:07 -0300

    feat: context test

--- src/test/java/com/jomasoares/pokecrud/PokecrudApplicationTests.java
+++ src/test/java/com/jomasoares/pokecrud/PokecrudApplicationTests.java
@@ -1,13 +1,17 @@
 package com.jomasoares.pokecrud;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class PokecrudApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertNotNull(context);
 	}
 
 }
