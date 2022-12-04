8ecf4f29cc0d74947c8cc863ce7874f3de18df14
jomasoares <jomamts@gmail.com>
2022-12-04 10:15:29 -0300

    fix: missing handler added

--- src/main/java/com/jomasoares/pokecrud/configs/ExceptionConfig.java
+++ src/main/java/com/jomasoares/pokecrud/configs/ExceptionConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.jomasoares.pokecrud.dtos.ExceptionResponseDto;
 import com.jomasoares.pokecrud.exceptions.BadRequestException;
+import com.jomasoares.pokecrud.exceptions.NotFoundException;
 
 /**
  * Custom exception handler.
@@ -18,7 +19,12 @@ public class ExceptionConfig {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ExceptionResponseDto> handleBadRequest(BadRequestException e) {
-		return new ResponseEntity<>(new ExceptionResponseDto(e.getMessage()), HttpStatus.BAD_REQUEST);
+		return defaultResponse(e, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ExceptionResponseDto> handleNotFound(NotFoundException e) {
+		return defaultResponse(e, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
@@ -27,5 +33,9 @@ public class ExceptionConfig {
 		ExceptionResponseDto body = new ExceptionResponseDto("Unkown error. Try again later.");
 		return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+
+    private ResponseEntity<ExceptionResponseDto> defaultResponse(Exception e, HttpStatus s) {
+        return new ResponseEntity<>(new ExceptionResponseDto(e.getMessage()), s);
+    }
     
 }
