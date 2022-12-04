5a959b8eaf3aa33ec3ca7c41d63cb509cb72ae75
jomasoares <jomamts@gmail.com>
2022-12-04 10:47:17 -0300

    fix: add missing constructor

--- src/main/java/com/jomasoares/pokecrud/models/Pokemon.java
+++ src/main/java/com/jomasoares/pokecrud/models/Pokemon.java
@@ -7,12 +7,14 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Basic pokemon representation.
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
 public class Pokemon {
